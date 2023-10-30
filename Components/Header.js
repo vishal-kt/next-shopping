97d29752b62ee9b76d3e484953fb025161d71b5c
VISHAL <vishalstar895@gmail.com>
2023-10-30 16:16:58 +0530

    next update

---
+++ Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='hder px-5 bg-green-500 hover:text-red-500  flex items-center justify-between'>
+        <h2>Vishal Kumar</h2>
+        <div className='flex gap-8'>
+            <h2>Project</h2>
+            <h2>About</h2>
+            <h2>About</h2>
+            <h2>About</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
-
 "use client"
 import React, { useState } from "react";
+import Header from "@/Components/Header"
 const page =()=>{ 
  let [marks , setMarks] =useState(80)
   return (
   <div>
+
+    <Header/>
     <h1>My Score is {marks} </h1>
     <button id='btn'  onClick={()=>{
       setMarks(92)
     }}>Change</button>
+
   </div>
   )
 }
