db45c165ed5821348e17a955ad02b2a88f823ab9
VISHAL <vishalstar895@gmail.com>
2023-10-30 12:06:48 +0530

    next project

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div>Page</div>
+      <div id='main'>
+      <div id='nav'>Page</div>
+      </div>
+     
     </div>
   )
 }
